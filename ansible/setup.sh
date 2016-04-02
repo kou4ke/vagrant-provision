9b653d36055c89c538aa236c2d721b4e5e2e3f0f
kou4ke <kousuke6122@gmail.com>
2016-04-03 08:57:54 +0900

    add ansible install

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -13,19 +13,13 @@ if [ ! -f $ANSIBLE_TMP_DIR/$ANSIBLE_HOSTS ]; then
 fi
  
 if ! [ `which ansible` ]; then
-  #  wget http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
-  #  sudo rpm --import RPM-GPG-KEY-EPEL-6
-  #  wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-  #  sudo rpm -ivh epel-release-6-8.noarch.rpm
-  #   sudo yum install -y ansible
   # for centos/7
   sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
   sudo yum -y update
   sudo yum install -y ansible
 fi
-#  
-# cp /vagrant/${ANSIBLE_HOSTS} ${TEMP_HOSTS}
-# chmod -x ${TEMP_HOSTS}
-# echo "Running Ansible"
-# bash -c "ansible-playbook /vagrant/${ANSIBLE_PLAYBOOK} --inventory-file=${TEMP_HOSTS} --connection=local"
-# rm ${TEMP_HOSTS}
+  
+cd ${ANSIBLE_TMP_DIR}
+chmod -x ${ANSIBLE_TMP_DIR}
+echo "Running Ansible"
+bash -c "ansible-playbook ${ANSIBLE_PLAYBOOK} -i ${ANSIBLE_HOSTS} --connection=local"
